b230a12ee38091082a44a00576088654a5b51314
ch-praneeth-08 <praneethreddychintakunta@gmail.com>
2025-02-07 20:16:27 +0530

    added routes for creating, updating , deleting and displaying users from in program stored data

--- server.js
+++ server.js
@@ -1,22 +1,99 @@
 const express = require("express")
 const app = express();
+app.use(express.json());
+const users = [
+    {
+        id: 1, name: "sharan", age: 18
+    },
+    {
+        id: 2, name: "praneeth", age: 20
+    },
+    {
+        id: 3, name: "prachod", age: 19
+    },
+    {
+        id: 4, name: "krishna", age: 21
+    },
+    {
+        id: 5, name: "haradeep", age: 20
+    }
+]
 
-app.get("/",(req,res)=>{
-    res.send({message : "This is get request handler.."})
+app.get("/users", (req, res) => {
+    res.send({ msg: "LIST of users", payload: users })
 })
 
-app.put("/",(req,res)=>{
-    res.send({message:"This is put request handler"})
+app.get("/user/:id", (req, res) => {
+
+    const userId = Number(req.params.id)
+
+    if (Number.isInteger(userId) && userId > 0) {
+        const result = users.find(user => user.id == userId)
+        if (result == undefined) {
+            res.send({ message: "user is not available" })
+        }
+        else {
+            res.send({ message: "user Details", payload: result })
+        }
+    }
+    else{
+        res.send({message:"enter a valid id number"})
+    }
+
 })
 
-app.post("/",(req,res)=>{
-    res.send({message:"This is post request handler"})
+app.post("/user",(req,res)=>{
+    const {id, name , age} = req.body;
+     if(!id || !name|| !age){
+        return res.status(400).send({message:"all inputs are required"})
+     }
+
+    const newid = Number(id);
+    if(!Number.isInteger(newid)|| newid<=0){
+        return res.status(400).send({message:"Id must be greater than 0"})
+        
+    }
+    const result = users.find(user=> user.id === newid)
+    if(result){
+        return res.status(400).send({message:"User already exists"})
+    }
+    users.push({id:newid , name :name , age:age})
+    res.send({message:"New user created",payload:{id, name, age}})
 })
 
-app.delete("/",(req,res)=>{
-    res.send({message:"This is a delete request handler"})
+app.put("/user",(req,res)=>{
+    const {id, name , age} = req.body;
+    
+     if(!id || !name|| !age){
+        return res.status(400).send({message:"all inputs are required"})
+     }
+
+    const newid = Number(id);
+    if(!Number.isInteger(newid)|| newid<=0){
+        return res.status(400).send({message:"Id must be greater than 0"})
+    }
+    const Userindex = users.findIndex(user=> user.id === newid)
+    if(Userindex==-1){
+        return res.status(400).send({message:"User doesnt exist"})
+    }
+    else{
+        users[Userindex] = { id: newid, name, age };
+        res.send({message:"user updated"})
+    }
 })
 
-app.listen(3000,()=>{
+app.delete("/users/:id",(req,res)=>{
+    const userId = req.params.id;
+    const newid = Number(userId);
+    const Userindex = users.findIndex(user=> user.id === newid)
+    if(Userindex==-1){
+        return res.status(400).send({message:"User doesnt exist"})
+    }
+    else{
+        users.splice(Userindex,1)
+        res.send({message:"user deleted"})
+    }
+})
+app.listen(3000, () => {
     console.log("sever running on port 3000....")
 })
