9253f4ea5d97c95b49d3ce3daa23878f6e52d796
ch-praneeth-08 <praneethreddychintakunta@gmail.com>
2025-02-07 18:05:14 +0530

    added rest client for sending requests to backend

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 const express = require("express")
-const app = express();  
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send({message : "This is get request handler.."})
+})
+
+app.put("/",(req,res)=>{
+    res.send({message:"This is put request handler"})
+})
+
+app.post("/",(req,res)=>{
+    res.send({message:"This is post request handler"})
+})
+
+app.delete("/",(req,res)=>{
+    res.send({message:"This is a delete request handler"})
+})
+
+app.listen(3000,()=>{
+    console.log("sever running on port 3000....")
+})
