88ca66752350a5b364c8a296de051ff7d28e02f2
ch-praneeth-08 <praneethreddychintakunta@gmail.com>
2025-02-10 22:38:12 +0530

    integrated database

--- server.js
+++ server.js
@@ -1,99 +1,39 @@
-const express = require("express")
-const app = express();
+const express = require("express");
+const { MongoClient } = require("mongodb");
+
+const app = express(); // default export
 app.use(express.json());
-const users = [
-    {
-        id: 1, name: "sharan", age: 18
-    },
-    {
-        id: 2, name: "praneeth", age: 20
-    },
-    {
-        id: 3, name: "prachod", age: 19
-    },
-    {
-        id: 4, name: "krishna", age: 21
-    },
-    {
-        id: 5, name: "haradeep", age: 20
-    }
-]
 
-app.get("/users", (req, res) => {
-    res.send({ msg: "LIST of users", payload: users })
-})
+const mongoURL = "mongodb://localhost:27017";
 
-app.get("/user/:id", (req, res) => {
+async function startServer() {
+    try {
+        const mclient = await MongoClient.connect(mongoURL);
+        console.log("DB connection success....");
 
-    const userId = Number(req.params.id)
+        const obj = mclient.db("practice");
+        const usersobj = obj.collection("users");
+        const productsobj = obj.collection("products");
 
-    if (Number.isInteger(userId) && userId > 0) {
-        const result = users.find(user => user.id == userId)
-        if (result == undefined) {
-            res.send({ message: "user is not available" })
-        }
-        else {
-            res.send({ message: "user Details", payload: result })
-        }
-    }
-    else{
-        res.send({message:"enter a valid id number"})
-    }
+        // Store collections in app locals for access in routes
+        app.set("usersobj", usersobj);
+        app.set("productsobj", productsobj);
 
-})
+        // Import routes after DB connection is established
+        const userapi = require("./APIs/userapidb.js");
+        const productApi = require("./APIs/productsapi.js");
 
-app.post("/user",(req,res)=>{
-    const {id, name , age} = req.body;
-     if(!id || !name|| !age){
-        return res.status(400).send({message:"all inputs are required"})
-     }
+        app.use("/user-api", userapi);
+        app.use("/product-api", productApi);
 
-    const newid = Number(id);
-    if(!Number.isInteger(newid)|| newid<=0){
-        return res.status(400).send({message:"Id must be greater than 0"})
-        
-    }
-    const result = users.find(user=> user.id === newid)
-    if(result){
-        return res.status(400).send({message:"User already exists"})
-    }
-    users.push({id:newid , name :name , age:age})
-    res.send({message:"New user created",payload:{id, name, age}})
-})
+        app.listen(3000, () => {
+            console.log("Server running on port 3000...");
+        });
 
-app.put("/user",(req,res)=>{
-    const {id, name , age} = req.body;
-    
-     if(!id || !name|| !age){
-        return res.status(400).send({message:"all inputs are required"})
-     }
+    } catch (err) {
+        console.error("Error in connecting to DB:", err);
+        process.exit(1); // Exit process on DB connection failure
+    }
+}
 
-    const newid = Number(id);
-    if(!Number.isInteger(newid)|| newid<=0){
-        return res.status(400).send({message:"Id must be greater than 0"})
-    }
-    const Userindex = users.findIndex(user=> user.id === newid)
-    if(Userindex==-1){
-        return res.status(400).send({message:"User doesnt exist"})
-    }
-    else{
-        users[Userindex] = { id: newid, name, age };
-        res.send({message:"user updated"})
-    }
-})
-
-app.delete("/users/:id",(req,res)=>{
-    const userId = req.params.id;
-    const newid = Number(userId);
-    const Userindex = users.findIndex(user=> user.id === newid)
-    if(Userindex==-1){
-        return res.status(400).send({message:"User doesnt exist"})
-    }
-    else{
-        users.splice(Userindex,1)
-        res.send({message:"user deleted"})
-    }
-})
-app.listen(3000, () => {
-    console.log("sever running on port 3000....")
-})
+startServer();
